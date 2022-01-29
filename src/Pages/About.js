67c21fb9870055bff8793def6b529c5bcfd234e2
Administrator <marufuzzaman7@gmail.com>
2022-01-29 22:36:08 +0600

    Finally Complete Navbar

---
+++ src/Pages/About.js
@@ -0,0 +1,8 @@
+const About = () =>{
+    return(
+        <>
+            This is About Page................
+        </>
+    )
+};
+export default About;
--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from "react-router-dom";
+import Home from "../Pages/Home";
+import Service from "../Pages/Service";
+import About from "../Pages/About";
 class NavbarComp extends Component {
     render() {
         return (
+            <Router>
             <div>
                 <Navbar bg="light" expand="lg">
                     
@@ -11,8 +15,10 @@ class NavbarComp extends Component {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/service">Service</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -20,11 +26,20 @@ class NavbarComp extends Component {
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
+                            
                         </Nav>
                         </Navbar.Collapse>
                     
                 </Navbar>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/service" element={<Service />} />
+                </Routes>
             </div>
+            <Outlet />
+            </Router>
+            
         );
     }
 }
